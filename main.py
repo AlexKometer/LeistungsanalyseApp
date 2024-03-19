f934a090b9e118c4cdb7fadfe97aadcd68c845a3
Alexander Kometer <162452412+AlexKometer@users.noreply.github.com>
2024-03-19 11:36:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,55 @@
+import json
 from my_function import build_person, build_experiment
 
 if __name__ == "__main__":
-    supervisor = build_person("Alexander", "Kometer", "male", 27)
-    subject = build_person("Max", "Mustermann", "male", 30)
+    print()
+    print("Welcome to the experiment builder!")
+    print("Please enter the following information about the experiment")
+    print()
+    experiment_name = input("Enter the experiment name: "),
+    experiment_date = input("Enter the date(dd-mm-yyyy): ")
+    while True:
+        check = input("Do you want to enter new supervisor information? (y/n): ")
+
+        if check == "y":
+            print()
+            print("Please enter the following information about the experiment supervisor!")
+            supervisor = build_person(
+                input("Enter the supervisor's first name: "),
+                input("Enter the supervisor's last name: "),
+                input("Enter the supervisor's gender: "),
+                int(input("Enter the supervisor's age: "))
+            )
+            break
+        elif check == "n":
+            supervisor = build_person("Alexander", "Kometer", "male", 27)
+            break
+        else: 
+            print("Please enter a valid input!")
+        
+
+    
+    print()
+    print("Please enter the following information about the experiment subject!")
+    print()
+    subject = build_person(
+        input("Enter the subject's first name: "),
+        input("Enter the subject's last name: "),
+        input("Enter the subjects gender: "),
+        int(input("Enter the subjects age: "))
+        )
+        
     
     experiment = build_experiment(
-        "Heart Rate Response to Exercise",
-        "2024-03-18",
+        experiment_name,
+        experiment_date,
         supervisor,
         subject
     )
-    
     print(experiment)
+
+
+with open("experiment.json", "w") as outfile:
+        json.dump(experiment, outfile, indent=4)
+        print("Experiment saved to experiment.json")
+        
